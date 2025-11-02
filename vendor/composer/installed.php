6ea1c935fad3b3be23e50c3077073dde88546d64
Marco Di Bella <m_dibella@icloud.com>
2025-11-02 15:57:14 +0100

    Try different file names to support composers built in class loader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mdibella-dev/wordpress-helper',
+        'pretty_version' => '1.2.0',
+        'version' => '1.2.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mdibella-dev/wordpress-helper' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
